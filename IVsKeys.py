67bca77f800d4a6012a8bf29f287bee4d44dcfe2
shreyastalole <48254943+shreyastalole@users.noreply.github.com>
2021-04-13 17:14:35 +0530

    Update IVsKeys.py

--- IVsKeys.py
+++ IVsKeys.py
@@ -4,7 +4,7 @@ from cryptography.fernet import Fernet
 def generateIV():
 	iv1 = os.urandom(16)
 	iv2 = os.urandom(8)
-	f=open(os.path.join(os.getcwd()+"\Infos",'IV.txt'),'wb')
+	f=open(os.path.join(os.getcwd()+"/Infos",'IV.txt'),'wb')
 	f.write(iv1)
 	f.write(b"::::")
 	f.write(iv2)
@@ -14,26 +14,26 @@ def generateIV():
 def generateKey():
 	key1 = os.urandom(16)
 	key2 = Fernet.generate_key()
-	f=open(os.path.join(os.getcwd()+"\Infos",'KEY1.txt'),'wb')
+	f=open(os.path.join(os.getcwd()+"/Infos",'KEY1.txt'),'wb')
 	f.write(key1)
 	f.close()
-	f=open(os.path.join(os.getcwd()+"\Infos",'KEY2.txt'),'wb')
+	f=open(os.path.join(os.getcwd()+"/Infos",'KEY2.txt'),'wb')
 	f.write(key2)
 	f.close()
 	return key1,key2
 
 def FetchIV():
-	f=open(os.path.join(os.getcwd()+"\Infos",'IV.txt'),'rb')
+	f=open(os.path.join(os.getcwd()+"/Infos",'IV.txt'),'rb')
 	cont=f.read()
 	f.close()
 	iv=cont.split(b"::::")
 	return iv
 
 def FetchKey():
-	f=open(os.path.join(os.getcwd()+"\Infos",'KEY1.txt'),'rb')
+	f=open(os.path.join(os.getcwd()+"/Infos",'KEY1.txt'),'rb')
 	key1=f.read()
 	f.close()
-	f=open(os.path.join(os.getcwd()+"\Infos",'KEY2.txt'),'rb')
+	f=open(os.path.join(os.getcwd()+"/Infos",'KEY2.txt'),'rb')
 	key2=f.read()
 	f.close()
 	return key1,key2
