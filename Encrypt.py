5005ee23fee4ad6de21257ded0a5e2a0832143b8
shreyastalole <48254943+shreyastalole@users.noreply.github.com>
2021-04-13 17:19:31 +0530

    Update Encrypt.py

--- Encrypt.py
+++ Encrypt.py
@@ -3,7 +3,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.fernet import Fernet
 import os
 def AES(key,iv):
-    f=open(os.path.join(os.getcwd()+"\Segments","0.txt"),"r")
+    f=open(os.path.join(os.getcwd()+"/Segments","0.txt"),"r")
     content=f.read()
     f.close()
     content=content.encode()
@@ -16,13 +16,13 @@ def AES(key,iv):
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
     encryptor = cipher.encryptor()
     cont = encryptor.update(content) + encryptor.finalize()
-    open(os.path.join(os.getcwd()+"\Segments","0.txt"),"wb").close()
-    f=open(os.path.join(os.getcwd()+"\Segments","0.txt"),"wb")
+    open(os.path.join(os.getcwd()+"/Segments","0.txt"),"wb").close()
+    f=open(os.path.join(os.getcwd()+"/Segments","0.txt"),"wb")
     f.write(cont)
     f.close();
 
 def BlowFish(key,iv):
-    f=open(os.path.join(os.getcwd()+"\Segments","1.txt"),"r")
+    f=open(os.path.join(os.getcwd()+"/Segments","1.txt"),"r")
     content=f.read()
     f.close()
     content=content.encode()
@@ -35,14 +35,14 @@ def BlowFish(key,iv):
     cipher = Cipher(algorithms.Blowfish(key), modes.CBC(iv), backend=backend)
     encryptor = cipher.encryptor()
     cont = encryptor.update(content) + encryptor.finalize()
-    open(os.path.join(os.getcwd()+"\Segments","1.txt"),"w").close()
-    f=open(os.path.join(os.getcwd()+"\Segments","1.txt"),"wb")
+    open(os.path.join(os.getcwd()+"/Segments","1.txt"),"w").close()
+    f=open(os.path.join(os.getcwd()+"/Segments","1.txt"),"wb")
     f.write(cont);
     f.close();
 
 
 def TrippleDES(key,iv):
-    f=open(os.path.join(os.getcwd()+"\Segments","2.txt"),"r");
+    f=open(os.path.join(os.getcwd()+"/Segments","2.txt"),"r");
     content=f.read();
     f.close();
     content=content.encode()
@@ -55,13 +55,13 @@ def TrippleDES(key,iv):
     cipher = Cipher(algorithms.TripleDES(key), modes.CBC(iv), backend=backend);
     encryptor = cipher.encryptor();
     cont = encryptor.update(content) + encryptor.finalize();
-    open(os.path.join(os.getcwd()+"\Segments","2.txt"),"w").close();
-    f=open(os.path.join(os.getcwd()+"\Segments","2.txt"),"wb");
+    open(os.path.join(os.getcwd()+"/Segments","2.txt"),"w").close();
+    f=open(os.path.join(os.getcwd()+"/Segments","2.txt"),"wb");
     f.write(cont);
     f.close();
 
 def IDEA(key,iv):
-	f=open(os.path.join(os.getcwd()+"\Segments","3.txt"),"r")
+	f=open(os.path.join(os.getcwd()+"/Segments","3.txt"),"r")
 	content=f.read()
 	f.close()
 	content=content.encode()
@@ -74,20 +74,20 @@ def IDEA(key,iv):
 	cipher = Cipher(algorithms.IDEA(key), modes.CBC(iv), backend=backend)
 	encryptor = cipher.encryptor()
 	cont = encryptor.update(content) + encryptor.finalize()
-	open(os.path.join(os.getcwd()+"\Segments","3.txt"),"w").close()
-	f=open(os.path.join(os.getcwd()+"\Segments","3.txt"),"wb")
+	open(os.path.join(os.getcwd()+"/Segments","3.txt"),"w").close()
+	f=open(os.path.join(os.getcwd()+"/Segments","3.txt"),"wb")
 	f.write(cont)
 	f.close()
 
 def EFernet(key):
-    f=open(os.path.join(os.getcwd()+"\Segments","4.txt"),"r")
+    f=open(os.path.join(os.getcwd()+"/Segments","4.txt"),"r")
     content=f.read()
     f.close()
     content=content.encode()
     fer = Fernet(key)
     content=fer.encrypt(content)
-    open(os.path.join(os.getcwd()+"\Segments","4.txt"),'w').close()
-    f=open(os.path.join(os.getcwd()+"\Segments","4.txt"),"wb")
+    open(os.path.join(os.getcwd()+"/Segments","4.txt"),'w').close()
+    f=open(os.path.join(os.getcwd()+"/Segments","4.txt"),"wb")
     f.write(content)
     f.close()
 
@@ -96,15 +96,15 @@ def HybridCryptKeys():
     f=open('Original.txt','wb')
     f.write(key)
     f.close()
-    listDir=os.listdir(os.getcwd()+"\Infos")
+    listDir=os.listdir(os.getcwd()+"/Infos")
     fer = Fernet(key)
     for i in listDir:
-        KI=open(os.getcwd()+'\Infos\\'+i,'rb')
+        KI=open(os.getcwd()+'/Infos//'+i,'rb')
         content=KI.read()
         KI.close()
         content=fer.encrypt(content)
-        open(os.path.join(os.getcwd()+"\Infos",i),'wb').close()
-        f=open(os.path.join(os.getcwd()+"\Infos",i),"wb")
+        open(os.path.join(os.getcwd()+"/Infos",i),'wb').close()
+        f=open(os.path.join(os.getcwd()+"/Infos",i),"wb")
         f.write(content)
         f.close()
 
